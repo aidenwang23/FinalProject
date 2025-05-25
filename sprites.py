b0afc4890fd142e303323e67695fdc0f0dd72ff5
oceanp8 <148895610+oceanp8@users.noreply.github.com>
2025-05-24 22:21:55 -0400

    new test platform and sprite change

--- sprites.py
+++ sprites.py
@@ -12,13 +12,20 @@ class Player:
         self.image = pygame.image.load(f"Sprites/Character/{state}.png")
         self.image = pygame.transform.scale_by(self.image, scale)
         self.image_size = self.image.get_size()
-        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.rect = self.image.get_rect(center=(self.x, self.y))
         self.surface = self.image
 
     def move(self, new_x, new_y):
         self.x = new_x
         self.y = new_y
-        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.rect = pygame.Rect(
+            self.x - self.image_size[0] // 2,
+            self.y - self.image_size[1] // 2,
+            self.image_size[0],
+            self.image_size[1]
+        )
+
+
 
     def position(self):
         return ((self.x - int(self.image_size[0] / 2)), (self.y - int(self.image_size[1] / 2)))
@@ -58,3 +65,12 @@ class Popup:
 
     def draw(self, screen):
         screen.blit(self.image, self.position())
+
+class Platform:
+    def __init__(self, x, y, scale):
+        self.image = pygame.image.load("Sprites/Misc/testPlatform.png")
+        self.image = pygame.transform.scale_by(self.image, scale)
+        self.rect = self.image.get_rect(topleft=(x, y))
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect.topleft)
